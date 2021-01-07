eb807872d89cd7d4b19b9e6ba7a219a9d3853132
LuisDiego10 <66862458+LuisDiego10@users.noreply.github.com>
2021-01-06 20:25:10 -0600

    Mergesort and doc

--- src/code/quicksort.py
+++ src/code/quicksort.py
@@ -1,23 +1,20 @@
 def Quicksort(list):
-    print("Lista: ",list)
-    if len(list) == 1 or len(list) == 0:
+    if len(list)<=1:
         return list
     else:
         pivot = list[0]
-        i = 0 #El i es utilizado como contador para cuando aparezca un elemento menor al pivot
-        #Recorre la lista
+        i = 0 #The i is used as a counter to carry out exchange operations
+        #Iterate the list
         for j in range(len(list)-1):
-            if list[j+1] < pivot:
+            if list[j+1] > pivot:
                 list[j+1],list[i+1] = list[i+1], list[j+1]
                 i += 1
 
-        list[0],list[i] = list[i],list[0]#Intercambiar el pivot por el último elemento menor encontrado
-        #Dividir la lista
+        list[0],list[i] = list[i],list[0] #Swap the pivot for the last minor element found
+        #Split the list
         firts = Quicksort(list[:i])
-        print("Firts",firts)
         second = Quicksort(list[i+1:])
-        print("Second",second)
         firts.append(list[i])
 
     return (firts + second)
-print(Quicksort([7,3,4]))
+print(Quicksort(["B","A","C"]))
